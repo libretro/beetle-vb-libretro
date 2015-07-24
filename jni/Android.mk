1c501ca367fafd04b7594f4152ee23d3e59e27ab
twinaphex <libretro@gmail.com>
2015-07-25 00:01:18 +0200

    Cleanups

--- jni/Android.mk
+++ jni/Android.mk
@@ -68,7 +68,6 @@ endif
 
 MEDNAFEN_SOURCES := $(MEDNAFEN_DIR)/mednafen.cpp \
 	$(MEDNAFEN_DIR)/error.cpp \
-	$(MEDNAFEN_DIR)/math_ops.cpp \
 	$(MEDNAFEN_DIR)/settings.cpp \
 	$(MEDNAFEN_DIR)/general.cpp \
 	$(MEDNAFEN_DIR)/state.cpp \
