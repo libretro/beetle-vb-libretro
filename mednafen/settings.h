50ce8b87f2585b8d6e56f2eebeee7cfb27dbf45f
celerizer <33245078+celerizer@users.noreply.github.com>
2018-01-20 23:16:51 -0600

    Right analog to digital - cleanup and additional settings

--- mednafen/settings.h
+++ mednafen/settings.h
@@ -6,6 +6,9 @@
 extern uint32_t setting_vb_lcolor;
 extern uint32_t setting_vb_rcolor;
 extern uint32_t setting_vb_anaglyph_preset;
+extern bool setting_vb_right_analog_to_digital;
+extern bool setting_vb_right_invert_x;
+extern bool setting_vb_right_invert_y;
 
 // This should assert() or something if the setting isn't found, since it would
 // be a totally tubular error!
